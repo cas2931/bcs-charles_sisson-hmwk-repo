c62f6ff3f7b756bff4ad0450fcf9f5cffd9c7ec8
Silver Snail 57 <cdarkly1392@gmail.com>
2020-02-27 17:41:48 -0500

    updated hmwk 3 js file, added assets folder to hmwk 3, updated links

---
+++ CS-HMWK3-PasswordGenerator/assets/passwordGNR8Rscript.js
@@ -0,0 +1,45 @@
+var inputLength 
+var confirmNumber 
+var confirmSpecAscii
+var confirmUpperCase 
+var confirmLowerCase 
+
+var number = ["1234567890"] 
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"] 
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"] 
+var specAscii = ["!#$%&()*+,-./:;<=>?@[\]^_{|}~"]
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword); 
+
+function generatePassword() {
+
+// Initial prompt for password length
+  inputLength= prompt("Please choose a length between 8 and 128 characters for your password");
+
+// If no value entered
+  if (!inputLength) {
+    alert("Value needed");
+  }
+  // Check if password length valid
+  else if (inputLength < 8 || inputLength > 128) {
+    prompt("Please choose between 8 and 128");
+  }  
+  // Confirm specific password characters
+  else {
+    confirmNumber= confirm("Will your password contain numbers?");
+    confirmSpecAscii= confirm("Will you password contain special characters?"); 
+    confirmUpperCase= confirm("Will your password contain uppercase letters?");
+    confirmLowerCase= confirm("Will your password contain lowercase letters?");
+  }
+}
