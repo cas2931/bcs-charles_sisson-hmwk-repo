fb4c1e9b5b9e01828de70e18f7a497e7e983cb61
Silver Snail 57 <cdarkly1392@gmail.com>
2020-03-04 21:07:48 -0500

    Added questions array to hmwk 4

--- CS-HMWK4-CodeQuiz/codeQuiz.js
+++ CS-HMWK4-CodeQuiz/codeQuiz.js
@@ -1,10 +1,63 @@
-var startButton = document.querySelector("#start-btn") 
-var quizDisplay= document.querySelector("#quiz-display") 
-var quizResults = document.querySelector("#quiz-results") 
+var startButton = document.getElementById("#start-btn") 
+var quizDisplay= document.getElementById("#quiz-display") 
+var quizResults = document.getElementById("#quiz-results") 
 
-var questionSet = []
+var questionSet = [
+    {
+        question: "In relation to information technology, what does API stand for?",
+            answers: {
+                a: "academic performance index",
+                b: "alphabet phonetique international",
+                c: "application programming interface",
+                d: "advance passenger information"
+                    },
+            correctAnswer: "c"
+    },
+    {
+        question: "Which API would allow a user to provide their current whereabouts to a web application?",   
+            answers: {
+                a: "geocity",
+                b: "geolocation", 
+                c: "geospot",
+                d: "geodestination"
+                    },
+            correctAnswer: "b"
+    }, 
+    {
+        question: "",
+            answers: {
+                a:"",
+                b:"",
+                c:"", 
+                d:""
+                    },
+            correctAnswer: ""
+    },
+    {
+        question: "",
+            answers: {
+                a:"",
+                b:"",
+            c:"", 
+            d:""
+                },
+        correctAnswer: ""
+    },
+    {
+        question: "",
+            answers: {
+                a:"",
+                b:"",
+                c:"", 
+                d:""
+                    },
+            correctAnswer: ""
+    },
+];
 var score = 0 
 
 startButton.addEventListener("click", quizTimer()) 
 
-quizTimer () 
+function quizTimer () 
+function showResults ()  
+function runQuiz ()
