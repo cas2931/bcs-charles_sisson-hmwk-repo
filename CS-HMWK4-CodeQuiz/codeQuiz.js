0a373515b261a30701323a27daa1bc59c741ec8d
Silver Snail 57 <cdarkly1392@gmail.com>
2020-03-07 17:49:05 -0500

    Extensive updates to hmwk 4 html & js files

--- CS-HMWK4-CodeQuiz/codeQuiz.js
+++ CS-HMWK4-CodeQuiz/codeQuiz.js
@@ -1,63 +1,238 @@
-var startButton = document.getElementById("#start-btn") 
-var quizDisplay= document.getElementById("#quiz-display") 
-var quizResults = document.getElementById("#quiz-results") 
+// Welcome div Elements 
+var welcomeEl = document.querySelector("#welcome");
+var startButton = document.querySelector("#startQuiz");
 
-var questionSet = [
-    {
-        question: "In relation to information technology, what does API stand for?",
-            answers: {
-                a: "academic performance index",
-                b: "alphabet phonetique international",
-                c: "application programming interface",
-                d: "advance passenger information"
-                    },
-            correctAnswer: "c"
-    },
-    {
-        question: "Which API would allow a user to provide their current whereabouts to a web application?",   
-            answers: {
-                a: "geocity",
-                b: "geolocation", 
-                c: "geospot",
-                d: "geodestination"
-                    },
-            correctAnswer: "b"
-    }, 
-    {
-        question: "",
-            answers: {
-                a:"",
-                b:"",
-                c:"", 
-                d:""
-                    },
-            correctAnswer: ""
-    },
-    {
-        question: "",
-            answers: {
-                a:"",
-                b:"",
-            c:"", 
-            d:""
-                },
-        correctAnswer: ""
-    },
-    {
-        question: "",
-            answers: {
-                a:"",
-                b:"",
-                c:"", 
-                d:""
-                    },
-            correctAnswer: ""
-    },
+//Quiz Page Elements 
+var quizEl = document.querySelector("#quiz");
+var questionEl = document.querySelector("#question");
+var answersEl = document.querySelector("#answers");
+
+//Input Score Page Elements 
+var inputScoreEl = document.querySelector("#inputScore");
+var initialsEl = document.querySelector("#initials");
+var submitInitialsBtnEl = document.querySelector("#submitInitials");
+var userScoreEl = document.querySelector("#score");
+
+//View High Scores Page Elements 
+var highScoresEl = document.querySelector("#highScores");
+var scoresEl = document.querySelector("#scores");
+var goBackBtnEl = document.querySelector("#goBack");
+var clearScoresBtnEl = document.querySelector("#clearScores");
+
+//Universal vars
+var viewHScoresBtnEl = document.querySelector("#viewHScores");
+var timerEl = document.querySelector("#timer");
+var score = 0;
+var currentQ = 0;
+var highScores = [];
+var interval;
+var timeGiven = 100;
+var secondsElapsed = 0;
+
+// Question Array
+var questions = [
+  {
+      title: "In relation to information technology, what does API stand for?",
+      choices: ["academic performance index",
+                "alphabet phonetique international",
+                "application programming interface",
+                "advance passenger information"],
+      answer: "application programming interface"
+  },
+  {
+    title: "Which built-in method removes the last element from an array and returns that element?",
+    choices: ["last()", "get()", "pop()", "None of the Above"],
+    answer: "pop()"
+  },
+  {
+      title: "Which API would allow a user to provide their current whereabouts to a web application?",
+      choices: ["geocity", "geolocation", "geospot", "geodestination"],
+      answer: "geolocation"
+  },
+  {
+      title: "Which built-in method returns the calling string value converted to lower case?",
+      choices: ["toLowerCase()", "toLower()", "changeCase(case)", "None of the Above"],
+      answer: "toLowerCase()"
+  },
+  {
+      title: "Which of the following function of Number object returns the number's value",
+      choices: ["toString()", "valueOf()", "toLocaleString()", "toPrecision()"],
+      answer: "valueOf()"
+  },
+  {
+      title: "Which of the following function of Array object joins all elements of an array into a string?",
+      choices: ["concat()", "join()", "pop()", "map()"],
+      answer: "join()"
+  }
 ];
-var score = 0 
 
-startButton.addEventListener("click", quizTimer()) 
+// Start timer f(x)
+function startTimer() {
+    timerEl.textContent = timeGiven;
+    interval = setInterval(function () {
+        secondsElapsed++;
+        timerEl.textContent = timeGiven - secondsElapsed;
+        if (secondsElapsed >= timeGiven) {
+            currentQ = questions.length;
+            nextQuestion();
+        }
+    }, 1000);
+}
 
-function quizTimer () 
-function showResults ()  
-function runQuiz ()
+// Stop timer f(x)
+function stopTimer() {
+    clearInterval(interval);
+}
+
+//Clears current question and calls for display of next question
+//Calls for input score display if last question
+function nextQuestion() {
+    currentQ++;
+    if (currentQ < questions.length) {
+        renderQuestion();
+    } else {
+        stopTimer();
+        if ((timeGiven - secondsElapsed) > 0)
+            score += (timeGiven - secondsElapsed);
+        userScoreEl.textContent = score;
+        hide(quizEl);
+        show(inputScoreEl);
+        timerEl.textContent = 0;
+    }
+}
+
+//checks answer based on current question and updates the user score
+function checkAnswer(answer) {
+    var answerIndex = answer.id - 1;
+
+    if (questions[currentQ].answer == questions[currentQ].choices[answerIndex]) {
+        score += 5;
+        displayMessage("Correct!");
+    }
+    else {
+        secondsElapsed += 10;
+        displayMessage("Wrong...");
+    }
+}
+
+//displays a message for 2 seconds
+function displayMessage(m) {
+    var timeStart = 0;
+    var timeStop = 2;
+    var messageHr = document.createElement("hr");
+    var messageEl = document.createElement("div");
+    messageEl.textContent = m;
+    document.querySelector(".jumbotron").appendChild(messageHr);
+    document.querySelector(".jumbotron").appendChild(messageEl);
+    var messageInterval = setInterval(function () {
+        timeStart++;
+        if (timeStart >= timeStop) {
+            messageHr.remove();
+            messageEl.remove();
+            clearInterval(messageInterval)
+        }
+    }, 1000);
+
+}
+
+//hides element
+function hide(element) {
+    element.style.display = "none";
+}
+
+//displays element
+function show(element) {
+    element.style.display = "block";
+
+}
+
+//reset local variables
+function reset() {
+    score = 0;
+    currentQ = 0;
+    secondsElapsed = 0;
+    timerEl.textContent = 0;
+}
+
+//=================== Rendering ================================
+
+//Renders current question
+function renderQuestion() {
+    questionEl.textContent = questions[currentQ].title;
+    for (i = 0; i < answersEl.children.length; i++) {
+        answersEl.children[i].children[0].textContent = (i + 1) + ": " + questions[currentQ].choices[i];
+    }
+}
+
+function renderHighScores() {
+    while (scoresEl.firstChild) {
+        scoresEl.removeChild(scoresEl.firstChild);
+    }
+    show(highScoresEl);
+    highScores = JSON.parse(localStorage.getItem("scores"));
+    for (var i = 0; i < highScores.length; i++) {
+        var scoreItem = document.createElement("div");
+        scoreItem.className += "row mb-3 p-2";
+        console.log(scoreItem)
+        scoreItem.setAttribute("style", "background-color:PaleTurquoise;");
+        scoreItem.textContent = (i + 1) + ". " + highScores[i].username + " - " + highScores[i].userScore;
+        scoresEl.appendChild(scoreItem);
+    }
+}
+
+//displays high scores
+viewHScoresBtnEl.addEventListener("click", function () {
+    hide(welcomeEl);
+    hide(quizEl);
+    hide(inputScoreEl);
+    renderHighScores();
+    stopTimer();
+    reset();
+});
+
+//starts quiz from  Welcome page
+startButton.addEventListener("click", function () {
+    hide(welcomeEl);
+    startTimer();
+    renderQuestion();
+    show(quizEl);
+});
+
+//Calls to check answer selected and calls to next question if button is clicked
+answersEl.addEventListener("click", function (e) {
+    if (e.target.matches("button")) {
+        checkAnswer(e.target);
+        nextQuestion();
+    }
+});
+
+//Creates a user score object to push to the local storage scores array calls to display high scores
+//calls to render high scores
+submitInitialsBtnEl.addEventListener("click", function () {
+    var initValue = initialsEl.value.trim();
+    if (initValue) {
+        var userScore = { username: initValue, userScore: score };
+        initialsEl.value = '';
+        if (localStorage.getItem("scores")) {
+            highScores = JSON.parse(localStorage.getItem("scores"));
+        }
+        highScores.push(userScore)
+        localStorage.setItem("scores", JSON.stringify(highScores));
+        hide(inputScoreEl);
+        renderHighScores();
+        reset();
+    }
+});
+
+//Goes back to Welcome page from High scores 
+goBackBtnEl.addEventListener("click", function () {
+    hide(highScoresEl);
+    show(welcomeEl);
+});
+
+//Clears saved scores from local storage
+clearScoresBtnEl.addEventListener("click", function () {
+    highScores = [];
+    localStorage.setItem("scores", JSON.stringify(highScores));
+    renderHighScores();
+});
