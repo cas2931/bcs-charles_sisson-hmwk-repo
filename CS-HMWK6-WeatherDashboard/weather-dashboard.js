667f611f151542a55bcc0fdcfba0a0db36cb9562
Silver Snail 57 <cdarkly1392@gmail.com>
2020-03-19 01:34:51 -0400

    Added hmwk6 folder, with index.html & js file for weather dashboard

---
+++ CS-HMWK6-WeatherDashboard/weather-dashboard.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+    var citySearch = $("#city-search-button"); 
+    var cityInput = $("#city-input-form").val();
+
+    var apiKey = "5f018959489e9fac40ea7e3b5269f86a";
+    var queryURL = "";
+
+    // Ajax call when citySearch clicked
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    //   })
+        
+    //     .then(function(response) {
+    
+});
