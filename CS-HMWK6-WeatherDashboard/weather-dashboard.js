5afe6a6619eebf8121937563654722e058a00b3a
Silver Snail 57 <cdarkly1392@gmail.com>
2020-03-21 22:06:34 -0400

    Added to hmwk6 js file & finalized index.html

--- CS-HMWK6-WeatherDashboard/weather-dashboard.js
+++ CS-HMWK6-WeatherDashboard/weather-dashboard.js
@@ -1,16 +1,15 @@
-$( document ).ready(function() {
-    var citySearch = $("#city-search-button"); 
-    var cityInput = $("#city-input-form").val();
+$(document).ready(function () {
 
     var apiKey = "5f018959489e9fac40ea7e3b5269f86a";
-    var queryURL = "";
+    var pastCities = [];
+    var cityInput = $("#city-input-form").val().trim();
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey + "&units=imperial"; // Last part for getting Fahreinheit temps
 
-    // Ajax call when citySearch clicked
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    //   })
-        
-    //     .then(function(response) {
-    
-});
+    $("#city-search-button").on("click", function() {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          })
+          .then(function (response){
+        console.log(response)
+          }) }) })
