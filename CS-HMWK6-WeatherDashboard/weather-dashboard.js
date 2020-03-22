47fd0c819a100655444df6e85339d2ec2350a56b
Charles Sisson <51415878+cas2931@users.noreply.github.com>
2020-03-21 22:10:28 -0400

    Update weather-dashboard.js

--- CS-HMWK6-WeatherDashboard/weather-dashboard.js
+++ CS-HMWK6-WeatherDashboard/weather-dashboard.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     var apiKey = "5f018959489e9fac40ea7e3b5269f86a";
     var pastCities = [];
     var cityInput = $("#city-input-form").val().trim();
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey + "&units=imperial"; // Last part for getting Fahreinheit temps
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey;
 
     $("#city-search-button").on("click", function() {
         $.ajax({
