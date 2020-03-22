9546b119c12702bfe7a3e24b2fa8061071128263
Silver Snail 57 <cdarkly1392@gmail.com>
2020-03-21 22:50:56 -0400

    Updated hmwk6 js file

--- CS-HMWK6-WeatherDashboard/weather-dashboard.js
+++ CS-HMWK6-WeatherDashboard/weather-dashboard.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-
+    var current_Date = moment().format('MMMM Do YYYY, h:mm:ss a');
     var apiKey = "5f018959489e9fac40ea7e3b5269f86a";
     var pastCities = [];
     var cityInput = $("#city-input-form").val().trim();
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey + "&units=imperial"; // Last part for getting Fahreinheit temps
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey; 
 
     $("#city-search-button").on("click", function() {
         $.ajax({
@@ -11,5 +11,16 @@ $(document).ready(function () {
             method: "GET"
           })
           .then(function (response){
-        console.log(response)
-          }) }) })
+        console.log(response);
+        // Fill out current weather conditions
+        $("#cityName").text(response.name); 
+        $("#currentDate").text(current_Date);  
+        $("#humidity").text("Humidity: " + response.main.humidity);
+        $("#windSpeed").text("Wind Speed: " + response.wind.speed);
+
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32; 
+        $("#temperature").text("Temperature (F) " + tempF.toFixed(2));
+
+          }) 
+     }) 
+})
