203602987fe0502fe573d6cb8027929923650878
Silver Snail 57 <cdarkly1392@gmail.com>
2020-04-07 16:32:45 -0400

    Installed inquirer & github-api npms in hmwk repo. Began laying out js code.

---
+++ CS-HMWK7-ReadmeGenerator/readmeGenerator.js
@@ -0,0 +1,18 @@
+var inquirer = require('inquirer');  
+var GitHub = require('github-api');
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
+
