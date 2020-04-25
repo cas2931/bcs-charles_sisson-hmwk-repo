72874a2b08a55b86d60119865bf59d889e9503aa
Silver Snail 57 <cdarkly1392@gmail.com>
2020-04-25 09:40:07 -0400

    Modified gitignore for hmwk9

--- CS-HMWK9-NoteTaker/server.js
+++ CS-HMWK9-NoteTaker/server.js
@@ -4,6 +4,17 @@ var path = require("path")
 var malone = express() 
 var PORT = process.env.PORT || 3000 
 
+malone.use(express.urlencoded({ extended: true }));
+malone.use(express.json()); 
+
+malone.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+malone.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
 malone.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
